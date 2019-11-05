34cf01637010e3363f5452a9f244c45e3f31c563
Daryl Magera <darylmagera@gmail.com>
2019-11-04 20:48:45 -0500

    working on contact form

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,46 @@ import './Contact.css'
 const Contact = () => {
     return (
         <section className="contact__container">
-            contact me
+             <h3 className="contact-header">
+                    Contact Me <span className="dots">....................<i className="fas fa-circle"></i></span><span className="num">05.</span>
+                </h3>
+                {/* <div className="contact-top"> */}
+                    <div className="contact-top">
+                        <div className="top-card">
+                            <div className="top-num">01.</div>
+                            <span className="top-icon"><i className="fas fa-map-marker-alt"></i></span>
+                            <span className="top-subtext">Location</span>
+                            <p className="top-text">Georgia, USA</p>
+                        </div>
+                    </div>
+
+                    <div className="contact-top">
+                         <div className="top-card">
+                            <div className="top-num">02.</div>
+                            <span className="top-icon"><i className="fas fa-envelope"></i></span>
+                            <span className="top-subtext">Email</span>
+                            <p className="top-text">mageradaryl12@gmail.com</p>
+                        </div>
+                    </div>
+
+                    <div className="contact-top">
+                         <div className="top-card">
+                            <div className="top-num">03.</div>
+                            <span className="top-icon"><i className="fas fa-phone-alt"></i></span>
+                            <span className="top-subtext">Phone</span>
+                            <p className="top-text">+1 470 778-7877</p>
+                        </div>
+                    </div>
+
+                <div className="contact-bottom">
+                    <form className="contact-form">
+                     <input type="text" placeholder="Name" required/>
+                     <input type="email" placeholder="Email" required/>
+                     <input type="text" placeholder="Subject" required/>
+                     <textarea type="text" placeholder="Your Message" required></textarea>
+                     <input type="submit" value="Send Message"/>
+                    </form>
+                </div>
         </section>
     )
 }
