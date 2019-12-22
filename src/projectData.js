5172a0abd64186589b6d0379ca49c3b8dc69238b
Daryl Magera <darylmagera@gmail.com>
2019-12-21 22:12:29 -0500

    added a new project called mooveaze

--- src/projectData.js
+++ src/projectData.js
@@ -1,41 +1,62 @@
 
 
 export const projects = [
-    {id: "project-0",
-     img: require('./components/resources/forkify.png'),
+
+   {id: "project-0",
+     img: require('./components/resources/mooveaze.png'),
      alt: "project-0" 
     },
 
     {id: "project-1",
-     img: require('./components/resources/budgety.png'),
+     img: require('./components/resources/forkify.png'),
      alt: "project-1" 
     },
 
     {id: "project-2",
-     img: require('./components/resources/omnifood.png'),
+     img: require('./components/resources/budgety.png'),
      alt: "project-2" 
     },
 
     {id: "project-3",
-     img: require('./components/resources/instagramclone.png'),
+     img: require('./components/resources/omnifood.png'),
      alt: "project-3" 
     },
 
     {id: "project-4",
-     img: require('./components/resources/lambdatimes.png'),
+     img: require('./components/resources/instagramclone.png'),
      alt: "project-4" 
     },
 
     {id: "project-5",
-     img: require('./components/resources/piggame.png'),
+     img: require('./components/resources/lambdatimes.png'),
      alt: "project-5" 
     },
+
+    // {id: "project-5",
+    //  img: require('./components/resources/piggame.png'),
+    //  alt: "project-5" 
+    // },
 
   ]
 
 
   export const projectDetail = [
+
     {id: "project-0",
+     title: "Mooveaze",
+     description: `Browse your favorite Movies, TV shows, and actors. Search for specific movies, shows or actors by date, rating, region and other categories. Browse the latest and greatest films and to find information about its actors, crew, and reviews. Rate and favorite Movies, TV shows and actors while having access to them through a user account. Login / Authentication.`,
+      features:`
+      ability to search for your favorite movies/shows,
+      ability to save your favorite movies/shows,
+      ability to create an account,`,
+     img: require('./components/resources/mooveaze-gif.gif'),
+     alt: "project-0",
+     num: 2.1,
+     siteLink: "https://www.mooveaze.cf/",
+     codeLink: "https://github.com/darmag12/Mooveaze"
+    },
+
+    {id: "project-1",
      title: "Forkify",
      description: `The Forkify app was developed for both cooks and people who don't know how to cook.
      It features;`,
@@ -45,13 +66,13 @@ export const projects = [
       ability to add a recipe to a shopping list which gives you a list of ingredients,
       No need of worrying about losing your favorite recipe list when you leave the application.`,
      img: require('./components/resources/forkify-gif.gif'),
-     alt: "project-0",
-     num: 2.1,
+     alt: "project-1",
+     num: 2.2,
      siteLink: "https://www.forkify.cf/",
      codeLink: "https://github.com/darmag12/forkify-app"
     },
 
-    {id: "project-1",
+    {id: "project-2",
      title: "Budgety",
      description: `Budgeting made easy. This app will help you manage your cash flow and also see how much    money you are earning and how much you spend on certain things.
         It features`,
@@ -60,13 +81,13 @@ export const projects = [
       calculates the percentage you spend in relation to your budget,
       Updates everything in real time`,
      img: require('./components/resources/budgety-gif.gif'),
-     alt: "project-1",
-     num: 2.2,
+     alt: "project-2",
+     num: 2.3,
      siteLink: "https://www.budgety.cf/",
      codeLink: "https://github.com/darmag12/Budgety-app"
     },
 
-    {id: "project-2",
+    {id: "project-3",
      title: "Omnifood",
      description: `This is a landing page for a food delivery company that delivers healthy food.
      This company targets people that have a tight schedule sometimes, making it hard to cook at home but still want to eat a healthy meal.
@@ -75,13 +96,13 @@ export const projects = [
      Smooth scrolling effects,
      Responsive`,
      img: require('./components/resources/omnifood-gif.gif'),
-     alt: "project-2",
-     num: 2.3,
+     alt: "project-3",
+     num: 2.4,
      siteLink: "https://www.omnifood.cf/",
      codeLink: "https://github.com/darmag12/Omnifood"
     },
 
-    {id: "project-3",
+    {id: "project-4",
      title: "Instagram Clone",
      description: `I made this app using react. The goal of this app was to follow a design pattern that was given(located in the readme file on gitHub).My task was to build everything from scratch using CRA. I will be adding more features in the future like the ability to add a post, etc.
      Available features include but not limited to;`,
@@ -91,13 +112,13 @@ export const projects = [
      ability to post/add comments,
      ability to logout`,
      img: require('./components/resources/instagramclone-gif.gif'),
-     alt: "project-3",
-     num: 2.4,
+     alt: "project-4",
+     num: 2.5,
      siteLink: "https://www.instagramclone.cf/",
      codeLink: "https://github.com/darmag12/React-Insta-Clone"
     },
 
-    {id: "project-4",
+    {id: "project-5",
      title: "Lambda Times",
      description: `This is a Lambda Newspaper. My job was to add functionality to the tabs below the 'Lambda Times' logo. Where by the tabs act as filters for articles, and when clicked on, they filter out all articles except those with that tag.
      It features`,
@@ -106,29 +127,29 @@ export const projects = [
      Includes a carosel which was one of the stretch goals of this project,
      Sticky navigation.`,
      img: require('./components/resources/lambdatimes-gif.gif'),
-     alt: "project-4",
-     num: 2.5,
+     alt: "project-5",
+     num: 2.6,
      siteLink: "http://www.lambdatimes.cf/",
      codeLink: "https://github.com/darmag12/Sprint-Challenge-Applied-Javascript"
     },
 
-    {id: "project-5",
-     title: "Pig Game",
-     description: `The sole purpose of the pig game was to practice javascript basics such as dom-manipulation, etc.
-     Game rules are as follows;
-     `,
-     features:`
-     The game has 2 players, playing in rounds
-     In each turn, a player rolls dice as many times as he wishes. Each result get added to his ROUND score
-     BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
-     The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
-     The first player to reach 100 points on GLOBAL score wins the game
-     Also, you can enter a desired winning score.`,
-     img: require('./components/resources/piggame-gif.gif'),
-     alt: "project-5",
-     num: 2.6,
-     siteLink: "http://www.piggame.cf",
-     codeLink: "https://github.com/darmag12/pig-game"
-    },
+    // {id: "project-5",
+    //  title: "Pig Game",
+    //  description: `The sole purpose of the pig game was to practice javascript basics such as dom-manipulation, etc.
+    //  Game rules are as follows;
+    //  `,
+    //  features:`
+    //  The game has 2 players, playing in rounds
+    //  In each turn, a player rolls dice as many times as he wishes. Each result get added to his ROUND score
+    //  BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+    //  The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+    //  The first player to reach 100 points on GLOBAL score wins the game
+    //  Also, you can enter a desired winning score.`,
+    //  img: require('./components/resources/piggame-gif.gif'),
+    //  alt: "project-5",
+    //  num: 2.6,
+    //  siteLink: "http://www.piggame.cf",
+    //  codeLink: "https://github.com/darmag12/pig-game"
+    // },
 
   ]
